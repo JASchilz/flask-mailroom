608fd4cf41c46496bea5cff7959df50000e7d921
Aron Watson <watsaro@gmail.com>
2020-04-06 21:18:39 -0700

    intinal commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Flask Mailroom Application
 
-Running at [http://afternoon-reef-51666.herokuapp.com/donations/](http://afternoon-reef-51666.herokuapp.com/donations/).
+Running at [http://protected-ravine-65283.herokuapp.com/donations/](http://protected-ravine-65283.herokuapp.com/donations/).
 
 ## Your Task
 
--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import os
-import base64
+import sys
 
-from flask import Flask, render_template, request, redirect, url_for, session
-
+import peewee
+from flask import Flask, render_template, request, redirect, url_for
 from model import Donation, Donor
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
 
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
 
-@app.route('/donate', methods=['GET', 'POST'])
+
+@app.route('/donate/', methods=['GET', 'POST'])
 def donate():
     if request.method == 'POST':
-        donor = Donor.select().where(Donor.name == request.values.get('name')).get()
-        donation = Donation(donor=donor, value=request.values.get('donation') )
-        donation.save()
+        try:
+            donor = Donor.select().where(Donor.name == request.values.get('name')).get()
+        except peewee.DoesNotExist:
+            donor = ""
+        except Exception as e:
+            print(f'{e}', file=sys.stderr)
+        if donor:
+            donation = Donation(donor=donor, value=request.values.get('donation'))
+            donation.save()
+        else:
+            new_donor = Donor(name=request.values.get('name'))
+            new_donor.save()
+            donation = Donation(donor=new_donor, value=request.values.get('donation'))
+            donation.save()
         return redirect(url_for('all'))
     else:
         return render_template('donate.jinja2')
 
-    
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
